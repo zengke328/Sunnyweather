06237037c53f460ad1b16654624fe9bd8713b015
zengke <zengke3282006@163.com>
2020-11-01 22:28:51 +0800

    kotlin插件版本升级，自动补全，retrofit完善

--- app/src/main/java/com/zk/sunnyweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/zk/sunnyweather/logic/network/PlaceService.kt
@@ -1,4 +1,14 @@
 package com.zk.sunnyweather.logic.network
 
+import com.zk.sunnyweather.SunnyWeatherApplication
+import com.zk.sunnyweather.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface PlaceService {
+
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+
 }
--- app/src/main/java/com/zk/sunnyweather/logic/network/ServiceCreator.kt
+++ app/src/main/java/com/zk/sunnyweather/logic/network/ServiceCreator.kt
@@ -1,4 +1,21 @@
 package com.zk.sunnyweather.logic.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ServiceCreator {
+
+
+    private const val BASE_URL = "https://api.caiyunapp.com/"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> create(serviceClass: Class<T>): T = retrofit.create(serviceClass)
+
+
+    //泛型实化
+    inline fun <reified T> create(): T = create(T::class.java)
+
 }
